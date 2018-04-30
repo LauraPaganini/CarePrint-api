2a6732bd40a2aea41f6e68ce677999060b3d3b1f
Ubuntu <ubuntu@ip-172-31-25-47.us-east-2.compute.internal>
2018-04-30 00:57:44 +0000

    detectText()

--- imageUpload.go
+++ imageUpload.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"encoding/json"
 
 	vision "cloud.google.com/go/vision/apiv1"
 	"golang.org/x/net/context"
@@ -38,3 +40,105 @@ func ImageUploadHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(label.Description)
 	}
 }
+func detectText() error {
+        
+    type billData struct {
+          hospitalName string `json:"hospitalName"`
+          treatmentDate string `json:"treatmentDate"`
+          medicalCode string `json:"medicalCode"`
+          serviceDescription string `json:"serviceDescription"`
+          balance string `json:"balance"`
+    }
+    build := ""
+    _ = build
+
+    ctx := context.Background()
+    testBill := billData{hospitalName: "", treatmentDate: "", medicalCode: "", serviceDescription: "", balance: ""}
+    client, err := vision.NewImageAnnotatorClient(ctx)
+            
+    if err != nil {
+        return err
+    }
+
+    filename := "CarePrint-api/testdata/realBill.jpg"
+    
+    f, err := os.Open(filename)
+    println(filename)
+
+    if err != nil {
+        return err
+    }
+
+    defer f.Close()
+    
+    image, err := vision.NewImageFromReader(f)
+    if err != nil {
+        if err != nil {
+            return err
+        }
+    }
+    annotations, err := client.DetectTexts(ctx, image, nil, 100)
+    if err != nil {
+        return err
+    }
+
+    if len(annotations) == 0 {
+        println("No text found.")
+    } else {
+        for index, annotation := range annotations {
+            //fmt.Println("\n", annotation.Description)
+            if (annotation.Description == "to:") {
+                for index2, annotation2 := range annotations {
+                    if index2 == index + 1 {
+                        build = annotation2.Description
+                    }
+                    if index2 == index + 2 {
+                        build += " " +  annotation2.Description
+                    }
+                    if index2 == index + 3 {
+                        build += " " + annotation2.Description
+                    }
+                    if index2 == index + 4 {
+                        build += " " + annotation2.Description
+                    }
+                    testBill.hospitalName = build
+                }
+            }
+            if (annotation.Description == "Service:") {
+                for index2, annotation2 := range annotations {
+                        if index2 == index + 1 {
+                            testBill.treatmentDate = annotation2.Description
+                        }
+                }
+            }
+            if (annotation.Description == "Code:") {
+                for index2, annotation2 := range annotations {
+                    if index2 == index + 1 {
+                        testBill.medicalCode = annotation2.Description
+                    }
+                }
+            }
+            if (annotation.Description == "Description:") {
+                for index2, annotation2 := range annotations {
+                    if index2 == index + 1 {
+                        testBill.serviceDescription = annotation2.Description + "I"
+                    }
+                }
+            }
+            if (annotation.Description == "Due") {
+                for index2, annotation2 := range annotations {
+                    if index2 == index + 1 {
+                        testBill.balance = annotation2.Description
+                    }
+                }
+            }
+        }
+    }
+//      fmt.Println(testBill)
+    b, err := json.Marshal(testBill)
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+    fmt.Println(b)
+    return  nil
+}    
